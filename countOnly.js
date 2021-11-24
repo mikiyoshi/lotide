c9dc7e13aecc45cca2bf1dbf3b260aff6440a333
mikiyoshi <mikiyoshi.kokura@gmail.com>
2021-11-24 20:07:42 +0000

    update countOnly.js

---
+++ countOnly.js
@@ -0,0 +1,60 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    // console.log('✅✅✅ Assertion Passed: ' + actual + ' === ' + expected);
+    //Template Literals (Template Strings)
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    // console.log('🛑🛑🛑 Assertion Failed: ' + actual + ' !== ' + expected);
+    //Template Literals (Template Strings)
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countOnly = function (allItems, itemsToCount) {
+  const results = {};
+
+  for (const item of allItems) {
+    console.log(item);
+    if (results[item]) {
+      results[item] += 1;
+    } else {
+      results[item] = 1;
+    }
+    if (itemsToCount[item]) {
+      if (results[item]) {
+        results[item] += 1;
+      } else {
+        results[item] = 1;
+      }
+    }
+  }
+
+  return results;
+};
+
+firstNames = [
+  'Karl',
+  'Salima',
+  'Agouhanna',
+  'Fang',
+  'Kavith',
+  'Jason',
+  'Salima',
+  'Fang',
+  'Joe',
+];
+
+const result1 = countOnly(firstNames, {
+  Jason: true,
+  Karima: true,
+  Fang: true,
+  Agouhanna: false,
+});
+
+assertEqual(result1['Jason'], 1);
+assertEqual(result1['Karima'], undefined);
+assertEqual(result1['Fang'], 2);
+assertEqual(result1['Agouhanna'], undefined);
+
+// TEST CODE
