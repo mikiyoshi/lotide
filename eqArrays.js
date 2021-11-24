b28e52714ae0528509077a4ec0f54684f76d1421
mikiyoshi <mikiyoshi.kokura@gmail.com>
2021-11-24 04:06:05 +0000

    new file of eqArrays.js

---
+++ eqArrays.js
@@ -0,0 +1,33 @@
+// FUNCTION IMPLEMENTATION
+const eqArrays = function (arrOne, arrTwo) {
+  // console.log(arrOne);
+  // console.log(arrTwo);
+  let result = 0;
+  for (let i = 0; i < arrOne.length; i++) {
+    if (arrOne[i] === arrTwo[i]) {
+      result += 0;
+    } else {
+      result += 1;
+    }
+  }
+  if (result === 0) {
+    return true;
+  } else {
+    return false;
+  }
+};
+const assertEqual = function (actual, expected) {
+  // console.log(actual);
+  // console.log(expected);
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// TEST CODE
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), true); // => should PASS
+assertEqual(eqArrays(['1', '2', '3'], ['1', '2', '3']), true); // => should PASS
+assertEqual(eqArrays(['1', '2', '3'], ['1', '2', 3]), true); // => should PASS
