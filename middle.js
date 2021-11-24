cf2232af797bd78b3aa7270c852aa436b06873a2
mikiyoshi <mikiyoshi.kokura@gmail.com>
2021-11-24 19:35:47 +0000

    this file update for task

---
+++ middle.js
@@ -0,0 +1,34 @@
+const eqArrays = function (arrOne, arrTwo) {
+  for (let i = 0; i < arrOne.length; i++) {
+    if (arrOne[i] !== arrTwo[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function (actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const middle = function (array) {
+  const middleIndex = Math.floor(array.length / 2);
+  if (array.length < 3) {
+    return [];
+  } else if (array.length % 2 === 1) {
+    return [array[middleIndex]];
+  } else if (array.length % 2 === 0) {
+    return [array[middleIndex - 1], array[middleIndex]];
+  }
+};
+
+console.log(middle([1])); // => []
+console.log(middle([1, 2])); // => []
+console.log(middle([1, 2, 3])); // => [2]
+console.log(middle([1, 2, 3, 4, 5])); // => [3]
+console.log(middle([1, 2, 3, 4])); // => [2, 3]
+console.log(middle([1, 2, 3, 4, 5, 6])); // => [3, 4]
