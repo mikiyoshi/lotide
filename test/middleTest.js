04ef22669744530bcbd4d5e342cca8156bd70fd7
mikiyoshi <mikiyoshi.kokura@gmail.com>
2021-12-07 00:12:42 +0000

    this file updated for new index.js

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,4 +1,12 @@
-const middle = require('./../middle.js');
-const assertArraysEqual = require('./../assertArraysEqual');
+// const middle = require('./../middle.js');
+// const assertArraysEqual = require('./../assertArraysEqual');
 
 //
+const assert = require('chai').assert;
+const middle = require('../middle');
+
+describe('#middle', () => {
+  it("returns '5' for ['5']", () => {
+    assert.deepEqual(middle([5]), []);
+  });
+});
